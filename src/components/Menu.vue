84983aa8f108efaf277ef4a6ecc50f007046d376
Matheus Alves Stela <matheus.alves.stela@gmail.com>
2021-03-23 17:05:15 -0300

    att

---
+++ src/components/Menu.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-card>
+      <v-tabs
+        fixed-tabs
+        background-color="black"
+        center-active
+        dark
+        >
+        <v-tab> Dados pessoais </v-tab>
+        <v-tab> Experiências </v-tab>
+        <v-tab> Formação acadêmica </v-tab>
+      </v-tabs>
+    </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'Menu'
+  }
+</script>
